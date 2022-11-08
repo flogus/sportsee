0447029221e24a163a30f2a0d2b33e0988ee5fb2
flogus <fg@motionery.com>
2022-11-08 14:10:00 +0100

    feat: add custom color for the widget

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,16 @@ module.exports = {
     extend: {
       colors: {
         'seesport': {
-          red: '#FF0000',
           darkgrey: '#282D30',
           lightgrey: '#FBFBFB',
+          red: '#FF0000',
+          lightred: 'rgba(255, 0, 0, 0.1)',
           blue: '#4AB8FF',
+          lightblue: 'rgba(74, 184, 255, 0.1)',
           yellow: '#F9CE23',
-          pink: '#FD5181'
+          lightyellow: 'rgba(249, 206, 35, 0.1)',
+          pink: '#FD5181',
+          lightpink: 'rgba(253, 81, 129, 0.1)'
       }
     },
     fontSize: {
