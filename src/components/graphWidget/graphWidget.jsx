d806a5fa9835c29e3a6a128ff4aac57c6f478c9d
flogus <fg@motionery.com>
2022-11-08 15:12:08 +0100

    fix: correct the width of the icons

--- src/components/graphWidget/graphWidget.jsx
+++ src/components/graphWidget/graphWidget.jsx
@@ -8,22 +8,22 @@ import IconProteine from '.././../assets/icon-widget-proteine.svg'
 function graphWidget(props) {
     if(props.widgettype === 'calories') {
         return(
-            <Widget texte="Calories" chiffre="1,930kCal" icon={IconCalorie} bgcolor="red"/>
+            <Widget texte="Calories" chiffre="1,930kCal" icon={IconCalorie} bgcolor="lightred"/>
         )
     }
     if(props.widgettype === 'proteines') {
         return(
-            <Widget texte="Proteines" chiffre="155g" icon={IconProteine} bgcolor="blue" />
+            <Widget texte="Proteines" chiffre="155g" icon={IconProteine} bgcolor="lightblue" />
         )
     }
     if(props.widgettype === 'glucides') {
         return(
-            <Widget texte="Glucides" chiffre="290g" icon={IconGlucide} bgcolor="yellow" />
+            <Widget texte="Glucides" chiffre="290g" icon={IconGlucide} bgcolor="lightyellow" />
         )
     }
     if(props.widgettype === 'lipides') {
         return(
-            <Widget texte="Lipides" chiffre="50g" icon={IconLipide} bgcolor="pink" />
+            <Widget texte="Lipides" chiffre="50g" icon={IconLipide} bgcolor="lightpink" />
         )
     }
 }
