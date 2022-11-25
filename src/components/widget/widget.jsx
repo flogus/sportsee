a27c4593b028054f9011bfc99c80d3dcde5c7b96
flogus <fg@motionery.com>
2022-11-25 14:55:25 +0100

    fix: correct the icon display in the widget

--- src/components/widget/widget.jsx
+++ src/components/widget/widget.jsx
@@ -4,9 +4,9 @@ function widget(props) {
   const bgcolor = `bg-seesport-${props.bgcolor}`;
   return (
     <div className="mx-4 mr-0 ml-6">
-      <div className="bg-seesport-lightgrey flex p-8 rounded-md">
-        <div className={`${bgcolor} p-3 flex justify-center items-center rounded-md`}>
-          <img src={props.icon} alt={props.texte} className="w-5" />
+      <div className="bg-seesport-lightgrey flex p-4 rounded-md">
+        <div className={`${bgcolor} p-3 flex-initial justify-center items-center rounded-md flex-1`}>
+          <img src={props.icon} alt={props.texte} className="w-5 min-w-min" />
         </div>
         <div className="flex flex-col pl-2 justify-content">
           <div className="text-l font-bold">{props.chiffre}</div>
