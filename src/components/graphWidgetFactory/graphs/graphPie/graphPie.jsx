035f428f108af1289111c1908061eecc5c9f4f7b
flogus <fg@motionery.com>
2022-12-12 21:56:25 +0100

    fix: add center on the pie and round corners

--- src/components/graphWidgetFactory/graphs/graphPie/graphPie.jsx
+++ src/components/graphWidgetFactory/graphs/graphPie/graphPie.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import {
-  PieChart, Pie, Sector, Cell, ResponsiveContainer,
+  PieChart, Pie, Sector, Cell, Label ,ResponsiveContainer,
 } from 'recharts';
 
 
 function graphPie(props) {
+  const middleText1 = props.data[0].value + "%"
+  const middleText2 = "de votre objectif"
+
   return (
     <ResponsiveContainer width="100%" height={250}>
       <PieChart width="100%" height={250}>
-      <text textAnchor="middle">tedxte</text>
+      <text x={15} y={30} fontWeight="bold">Score</text>
         <Pie
           data={props.data}
           dataKey="value"
@@ -17,14 +20,23 @@ function graphPie(props) {
           endAngle={180}
           cx="50%"
           cy="50%"
-          outerRadius={90}
+          outerRadius={93}
           innerRadius={80}
           fill="#FF0000"
-          label
-        />
+          cornerRadius={90}
+          >
+            <Label value={middleText1} dy={-10} position="center" style={{
+              fontSize: "32px",
+              fontWeight: "bold",
+            }} />
+            <Label value={middleText2} dy={16} position="center" style={{
+              fill: "#999999",
+            }}/>
+          </Pie>
       </PieChart>
     </ResponsiveContainer>
   );
 }
+
 
 export default graphPie;
