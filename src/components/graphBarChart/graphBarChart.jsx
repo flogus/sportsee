32a2cfd228fb6330f74600dbf482f3fe0aeb00ee
flogus <fg@motionery.com>
2022-12-13 08:34:04 +0100

    feat: add title for the bars

--- src/components/graphBarChart/graphBarChart.jsx
+++ src/components/graphBarChart/graphBarChart.jsx
@@ -13,15 +13,19 @@ export default class GraphBarChart extends PureComponent {
     return (
       <ResponsiveContainer height="100%">
         <BarChart
-          title="Activité quotidienne"
           width="100%"
           height={300}
           data={this.props.data}
         >
+        <text x={15} y={23} fontWeight="bold">Activité quotidienne</text>
           <CartesianGrid strokeDasharray="3 3" vertical={false} />
           <XAxis axisLine />
           <YAxis axisLine={false} orientation="right" />
-          <Tooltip />
+          <Tooltip contentStyle={{
+              background: "red",
+              color: "green",
+              fontSize: "12px",
+            }} />
           <Legend verticalAlign="top" align="right" iconType="circle" iconSize={10} />
           <Bar barSize={8} name="Poids (kg)" dataKey="kg" fill="#282D30" />
           <Bar barSize={8} name="Calories brûlées (kCal)" dataKey="kcal" fill="#E60000" />
