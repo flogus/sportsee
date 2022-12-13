103ce9d5d677c4cccdd462c523f9e16b6d49310b
flogus <fg@motionery.com>
2022-12-13 08:32:59 +0100

    fix: correct the icon width with lot of text

--- src/components/widget/widget.jsx
+++ src/components/widget/widget.jsx
@@ -5,7 +5,7 @@ function widget(props) {
   return (
     <div className="mx-4 mr-0 ml-6">
       <div className="bg-seesport-lightgrey flex p-4 rounded-md">
-        <div className={`${bgcolor} p-3 flex-initial justify-center items-center rounded-md flex-1`}>
+        <div className={`${bgcolor} p-3 flex-initial shrink-0 justify-center items-center rounded-md flex-1`}>
           <img src={props.icon} alt={props.texte} className="w-5 min-w-min" />
         </div>
         <div className="flex flex-col pl-2 justify-content">
