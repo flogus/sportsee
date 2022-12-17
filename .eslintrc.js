1c871e6d65af4233456fca1db62a3c09f704f7bf
flogus <fg@motionery.com>
2022-12-17 11:15:58 +0100

    feat: add Eslint Config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,24 @@
-module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-  ],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 'latest',
-  },
-  plugins: [
-    'react',
-  ],
-  rules: {
-    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
-    'no-console': 'off',
-    'treatUndefinedAsUnspecified': false,
-  },
-};
+// module.exports = {
+//   env: {
+//     browser: true,
+//     es2021: true,
+//   },
+//   extends: [
+//     'plugin:react/recommended',
+//     'airbnb',
+//   ],
+//   parserOptions: {
+//     ecmaFeatures: {
+//       jsx: true,
+//     },
+//     ecmaVersion: 'latest',
+//   },
+//   plugins: [
+//     'react',
+//   ],
+//   rules: {
+//     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+//     'no-console': 'off',
+//     'no-unused-vars': 0,
+//   },
+// };
