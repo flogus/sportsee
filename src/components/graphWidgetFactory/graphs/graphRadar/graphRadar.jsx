f60c6fab55e4bdc811d8c11e9f903799e4f4b48e
flogus <fg@motionery.com>
2023-01-04 11:37:02 +0100

    fix: change font-size and outerradius for a better integration in 1024 width

--- src/components/graphWidgetFactory/graphs/graphRadar/graphRadar.jsx
+++ src/components/graphWidgetFactory/graphs/graphRadar/graphRadar.jsx
@@ -11,19 +11,19 @@ import {
  */
 function graphRadar(props) {
   return (
-    <ResponsiveContainer width="100%" height={250}>
+    <ResponsiveContainer width="100%">
       <RadarChart
         cx="50%"
         cy="50%"
-        outerRadius="80%"
+        outerRadius="45%"
         data={props.data}
         margin={{
           top: 0, right: 0, left: 0, bottom: 0,
         }}
       >
         <PolarGrid />
-        <PolarAngleAxis dataKey="subject" stroke="#FFFFFF" tick={{ fontSize: 14 }} />
-        <PolarRadiusAxis tickLine={false} />
+        <PolarAngleAxis dataKey="subject" stroke="#FFFFFF" tick={{ fontSize: 10 }} />
+        <PolarRadiusAxis tickLine={false} tick={{ fontSize: 8 }}  />
         <Radar name="fullMark" dataKey="value" fill="red" fillOpacity={0.6} />
       </RadarChart>
     </ResponsiveContainer>
